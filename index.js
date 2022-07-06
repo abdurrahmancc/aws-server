19d8459e183ff54d748598399533d779f204a318
Abdur Rahman <abdurrahman.offical.63@gmail.com>
2022-07-06 20:31:28 +0600

    create aws server

---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+var jwt = require("jsonwebtoken");
+const port = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    credentials: true,
+    crossDomain: true,
+    origin: ["http://localhost:3000"],
+  })
+);
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.1fj60.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req?.headers?.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access...." });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+const run = async () => {
+  try {
+    await client.connect();
+    const usersCollection = client.db("professional-aws").collection("users");
+    // const imgCollection = client.db("professional-aws").collection("images");
+    const productsCollection = client.db("professional-aws").collection("products");
+
+    app.get("/user", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      console.log(decodedEmail);
+      const query = {};
+      const result = await usersCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/all-products", verifyJWT, async (req, res) => {
+      const query = {};
+      const result = await productsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/add-product", verifyJWT, async (req, res) => {
+      const info = req.body;
+      const result = await productsCollection.insertOne(info);
+      res.send(result);
+    });
+
+    app.put("/user/:email", async (req, res) => {
+      const user = req.params.email;
+      const info = req.body;
+      filter = { email: user };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: info,
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: user }, process.env.ACCESS_TOKEN, { expiresIn: "1d" });
+      res.send({ result, token });
+    });
+  } finally {
+    // client.close()
+  }
+};
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("running profession-aws");
+});
+
+app.listen(port, () => {
+  console.log("running port", port);
+});
