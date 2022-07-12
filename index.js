582022791abe36c695e78c14be0f57a53177ab41
Abdur Rahman <abdurrahman.offical.63@gmail.com>
2022-07-13 01:46:08 +0600

    update server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 var jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
@@ -52,15 +52,31 @@ const run = async () => {
       res.send(result);
     });
 
-    app.get("/all-products", verifyJWT, async (req, res) => {
+    app.get("/all-products", async (req, res) => {
       const query = {};
       const result = await productsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/product-details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.findOne(query);
       res.send(result);
     });
 
     app.post("/add-product", verifyJWT, async (req, res) => {
       const info = req.body;
       const result = await productsCollection.insertOne(info);
+      res.send(result);
+    });
+
+    //product-delete
+    app.delete("/product-delete/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
       res.send(result);
     });
 
